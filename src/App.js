ab22c94d7c63e21a06c7f16ab14ddd9d66aa7894
dyCodes <yusufdaudu51@gmail.com>
2024-04-26 17:21:30 +0100

    feat: implement Report page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { Routes, Route } from "react-router-dom";
 import { HomePage } from "./pages";
+import ReportPage from "./pages/Report/ReportPage";
 
 function App() {
   return (
     <>
       <Routes>
         <Route path="/home" element={<HomePage />} />
+        <Route path="/report" element={<ReportPage />} />
       </Routes>
     </>
   );
